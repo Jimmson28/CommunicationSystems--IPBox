51e9316f448c9b19f5b7055642d4fb45104c219e
Jimmson28 <dorszjacob@outlook.com>
2026-01-02 20:42:03 +0100

    Add files via upload

---
+++ ServerApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using ChatLogic;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("IPCHAT SERVER");
+        Console.WriteLine("IP:...");
+        string ipAddress = Console.ReadLine();
+        Console.WriteLine("PORT:...");
+        int port = int.Parse(Console.ReadLine());
+        Server server = new Server();
+        server.startServer(ipAddress, port);
+        Console.ReadLine();
+    }
+}
